4e4b9e4ec6ec750669101f59f14593b6b66df598
Riccardo <riccardograndicelli03@gmail.com>
2026-08-05 21:33:45 +0200

    Re-downloaded all papers using also secondary cathegory

---
+++ download/download_papers.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import pickle
+
+__author__=['Riccardo Grandicelli']
+__email__=['riccardograndicelli03@gmail.com']
+
+all_physics_topics = ["Accelerator Physics", "Applied Physics", "Atmospheric and Oceanic Physics", "Atomic and Molecular Clusters", 
+                      "Atomic Physics", "Biological Physics", "Chemical Physics", "Classical Physics", "Computational Physics", 
+                      "Data Analysis, Statistics and Probability", "Fluid Dynamics", "General Physics", "Geophysics", 
+                      "History and Philosophy of Physics", "Instrumentation and Detectors", "Medical Physics", "Optics",
+                        "Physics and Society", "Physics Education", "Plasma Physics", "Popular Physics", "Space Physics"]
+
+def download_papers():
+    papers = pd.DataFrame(columns = ['paper', 'authors', 'title', 'abstract', 'primary_cathegory', 'secondary_cathegory'])
+
+    for i in range(13):
+        with open("data/list_of_papers/all_papers_" + str(i) + "_list", 'rb') as file:
+            paper_id_list = pickle.load(file)
+
+        for k in range(len(paper_id_list)):
+            article_number = paper_id_list[k] #already a string
+            article_response = requests.get("https://arxiv.org/abs/" + article_number)
+            article_soup = BeautifulSoup(article_response.text, 'html.parser')
+
+            #find authors
+            authors_tag = article_soup.find_all('meta', attrs = {'name' : 'citation_author'})
+            authors = [tag.attrs['content'] for tag in authors_tag]
+
+            #find title
+            title = article_soup.find('meta', attrs = {'name' : 'citation_title'}).attrs['content']
+
+            #find abstract
+            abstract = article_soup.find('meta', attrs = {'name' : 'citation_abstract'}).attrs['content']
+
+            #find primary cathegory
+            cathegory_tag = article_soup.find('span', attrs = {'class' : 'primary-subject'})
+            cathegory = re.findall(r'(.*)\s\(', cathegory_tag.text)[0]
+
+            #find secondary cathegory
+            other_cathegories_tag = article_soup.find('td', attrs = {'class' : 'tablecell subjects'})
+            other_cathegories = re.findall(r';\s*([^()]+?)\s*\(', other_cathegories_tag.text)
+            secondary_cathegory = [topic for topic in other_cathegories if topic in all_physics_topics]
+            if not secondary_cathegory:
+                secondary_cathegory = cathegory
+            else:
+                secondary_cathegory = secondary_cathegory[0]
+
+            #save everything in a dataset
+            papers.loc[len(papers)] = [paper_id_list[k], authors, title, abstract, cathegory, secondary_cathegory]
+        print("Iteration")
+
+    papers.to_csv("data/all_papers.csv")
+
+
+if(__name__ == '__main__'):
+    download_papers()
