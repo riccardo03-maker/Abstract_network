55f3bd1161e637f613d8e01881d048ae1cb3484f
Riccardo <riccardograndicelli03@gmail.com>
2026-08-11 14:38:03 +0200

    Implemented clustering based on Fiedler eigenvector

--- networks/networks_analysis.py
+++ networks/networks_analysis.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from build_networks import build_adjacency_matrix
+from .build_networks import build_adjacency_matrix
 import networkx as nx
 import pandas as pd
 import numpy as np
 from scipy.sparse import load_npz
+from scipy.sparse.linalg import eigsh
 
 __author__=['Riccardo Grandicelli']
 __email__=['riccardograndicelli03@gmail.com']
@@ -80,5 +81,62 @@ def centrality_measures(network: str):
     dataset.to_csv(network[::-1].split('/', 1)[1][::-1] + "/centrality_measures.csv")
 
 
+def split_fiedler_eigenvector():
+    '''
+    Divide the abstract embeddings graph into communities using the sign of the components of the Fiedler eigenvector.
+
+    During the first iteration, the nodes with degree 0 are removed from the graph. Then the graph is divided into two parts following
+    the signs of the Fiedler eigenvector, and the two induced subgraphs are built from this division.
+
+    During the following iterations, the induced subgraph with the higher number of nodes is divided into two more parts following 
+    again the signs of the Fiedler eigenvector of that graph. This procedure is repeated until we have a total of 22 subgraphs (same
+    number as the topics of physics papers).
+
+    Returns
+    -------
+        subgraphs_list: list of nx.Graph
+            List of subgraphs obtained through Fiedler eigenvector division.
+    '''
+    full_graph = nx.from_scipy_sparse_array(load_npz("networks/results/abstract_embeddings/abstract_tfidf_adjacency_0_2.npz"))
+
+    #create list of topics and set them as node attributes
+    topics_list = [all_papers['primary_cathegory'][i] if all_papers['primary_cathegory'][i] in all_physics_topics 
+               else all_papers['secondary_cathegory'][i] for i in range(25877)]
+    topics_dictionary = dict(zip(list(range(25877)), topics_list))
+    nx.set_node_attributes(full_graph, topics_dictionary, name = "Topic")
+
+    #keep only the largest component and remove the other nodes
+    largest_component = max(nx.connected_components(full_graph), key=len)
+    G = full_graph.subgraph(largest_component).copy()
+
+    subgraphs_list = []
+    G_max = G
+    for _ in range(21): #after N iterations we have N+1 subgraphs
+        #create fiedler eigenvector
+        laplacian_matrix = nx.laplacian_matrix(G_max).tocsr()
+        _, evecs = eigsh(laplacian_matrix, k = 2, which ='SM', tol = 1e-6)
+        fiedler_vector = evecs[:, 1]
+
+        #create subgraphs following the sign of Fiedler eigenvector
+        subgraph_1 = [node for index, node in enumerate(list(G_max.nodes)) if fiedler_vector[index] > 0]
+        subgraph_2 = [node for index, node in enumerate(list(G_max.nodes)) if fiedler_vector[index] < 0]
+
+        #append the smallest subgraph to the list of subgraphs and keep the other one for the next iteration
+        if len(subgraph_1) >= len(subgraph_2):
+            G_min = G_max.subgraph(subgraph_2).copy()
+            G_max = G_max.subgraph(subgraph_1)
+        else:
+            G_min = G_max.subgraph(subgraph_1).copy()
+            G_max = G_max.subgraph(subgraph_2)
+        subgraphs_list.append(G_min)
+
+        print("Iteration")
+
+    #append the remaining subgraph to the list of subgraphs
+    subgraphs_list.append(G_max)
+    return subgraphs_list
+
+
 if(__name__ == '__main__'):
-    centrality_measures("networks/results/scale_free_network/scale_free_network.npz")
+    #centrality_measures("networks/results/scale_free_network/scale_free_network.npz")
+    split_fiedler_eigenvector()
