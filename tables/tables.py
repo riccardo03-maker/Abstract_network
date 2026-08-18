c9626457e09fe45b6a216ef0d82207d0a30ff2f8
Riccardo Grandicelli <riccardograndicelli03@gmail.com>
2026-08-18 17:46:13 +0200

    Created tables 3 and 4

--- tables/tables.py
+++ tables/tables.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from collections import Counter
 import subprocess
+import pickle
 
 __author__= ['Riccardo Grandicelli']
 __email__= ['riccardograndicelli03@gmail.com']
@@ -38,5 +39,33 @@ def table_connected_components_tf_idf():
     subprocess.run("cp tf_idf_network/results/abstract_embeddings/connected_components.csv tables/table_2.csv", shell = True)
 
 
+def table_of_cathegories_by_community():
+    '''
+    Create the fourth table of the report
+    '''
+    #open all list of communities obtained with the three algorithms
+    with open("tf_idf_network/results/abstract_embeddings/fiedler_split", 'rb') as file:
+        fiedler_subgraphs_list = pickle.load(file)
+    with open("tf_idf_network/results/abstract_embeddings/k_means_split", 'rb') as file:
+        kmeans_subgraphs_list = pickle.load(file)
+    with open("tf_idf_network/results/abstract_embeddings/louvain_split", 'rb') as file:
+        louvain_subgraphs_list = pickle.load(file)
+
+    #count the number of papers in each community for each algorithm
+    fiedler_cathegories_by_community = [len(community) for community in fiedler_subgraphs_list]
+    kmeans_cathegories_by_community = [len(community) for community in kmeans_subgraphs_list]
+    louvain_cathegories_by_community = [len(community) for community in louvain_subgraphs_list]
+
+    #append 3 strings "NE" (Not Existing) to the Louvain list, since it has only 19 communities instead of 22
+    louvain_cathegories_by_community += ["NE", "NE", "NE"]
+
+    #create the dataset: each row is a community, and each column is an algorithm
+    cathegories_by_community_dict = {"Fiedler" : fiedler_cathegories_by_community, "K_Means": kmeans_cathegories_by_community, 
+                                     "Louvain" : louvain_cathegories_by_community}
+    cathegories_by_community_data = pd.DataFrame(data = cathegories_by_community_dict)
+    cathegories_by_community_data.to_csv("tables/table_4.csv")
+
+
 if(__name__ == '__main__'):
-    table_connected_components_tf_idf()
+    #table_connected_components_tf_idf()
+    table_of_cathegories_by_community()
